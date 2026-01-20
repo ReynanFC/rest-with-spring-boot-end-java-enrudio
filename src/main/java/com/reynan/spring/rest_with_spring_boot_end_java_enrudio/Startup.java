96b2701a159e61a70b97f739fce1353cfa59191e
Reynan <reynandan11@gmail.com>
2026-01-20 09:26:45 -0300

    Creating the structure with Spring Boot

---
+++ src/main/java/com/reynan/spring/rest_with_spring_boot_end_java_enrudio/Startup.java
@@ -0,0 +1,13 @@
+package com.reynan.spring.rest_with_spring_boot_end_java_enrudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(Startup.class, args);
+	}
+}
---
+++ src/test/java/com/reynan/spring/rest_with_spring_boot_end_java_enrudio/RestWithSpringBootEndJavaEnrudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reynan.spring.rest_with_spring_boot_end_java_enrudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootEndJavaEnrudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
