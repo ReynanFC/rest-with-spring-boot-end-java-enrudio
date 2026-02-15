f43097000619d31bb4924b853c8d0c55492bb90d
Reynan <reynandan11@gmail.com>
2026-02-15 18:54:18 -0300

    Add Flyway configuration and Maven plugin

---
+++ src/main/resources/db/migration/V2__Porpulate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO person (address, first_name, gender, last_name) VALUES
+('Rua das Flores, 123','Carlos','Male','Silva'),
+('Av. Paulista, 900','Mariana','Female','Souza'),
+('Rua A, 45','João','Male','Oliveira'),
+('Rua B, 78','Fernanda','Female','Costa'),
+('Av. Brasil, 1500','Lucas','Male','Pereira'),
+('Rua Exemplo, 123 - São Paulo, SP','João','Masc','Silva'),
+('Rua X','Reynan','Male','Silva');
