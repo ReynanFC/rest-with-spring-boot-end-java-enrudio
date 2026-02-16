fcdb77991d735034db19a5d129cad878e05cd25d
Reynan <reynandan11@gmail.com>
2026-02-16 20:36:01 -0300

    Application Content Negotiation

--- src/main/java/com/reynan/spring/rest_with_spring_boot_end_java_enrudio/controllers/PersonController.java
+++ src/main/java/com/reynan/spring/rest_with_spring_boot_end_java_enrudio/controllers/PersonController.java
@@ -7,7 +7,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -17,37 +16,56 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}",
+            produces = {
+             MediaType.APPLICATION_JSON_VALUE,
+             MediaType.APPLICATION_XML_VALUE,
+             MediaType.APPLICATION_YAML_VALUE
+            })
     public PersonDTO findById(@PathVariable("id") Long id) {
 
         var person = personService.findById(id);
-        person.setBirthDay(LocalDate.now());
-        person.setGender("Male");
-//        person.setPhoneNumber("+55 (32) 94002-8922");
-        person.setPhoneNumber("");
-
         return person;
     }
 
-    @GetMapping(value = "/find-all", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/find-all",
+            produces =
+            {MediaType.APPLICATION_JSON_VALUE,
+             MediaType.APPLICATION_XML_VALUE,
+             MediaType.APPLICATION_YAML_VALUE})
     public List<PersonDTO> findAll() {
+
         return personService.findAll();
     }
 
     @PostMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
+            consumes =
+                {MediaType.APPLICATION_JSON_VALUE,
+                 MediaType.APPLICATION_XML_VALUE,
+                 MediaType.APPLICATION_YAML_VALUE},
+            produces =
+                {MediaType.APPLICATION_JSON_VALUE,
+                 MediaType.APPLICATION_XML_VALUE,
+                 MediaType.APPLICATION_YAML_VALUE}
     )
     public PersonDTO create(@RequestBody PersonDTO personDTO) {
         return personService.create(personDTO);
     }
 
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(
+            consumes =
+            {MediaType.APPLICATION_JSON_VALUE,
+             MediaType.APPLICATION_XML_VALUE,
+             MediaType.APPLICATION_YAML_VALUE})
     public PersonDTO update(@RequestBody PersonDTO personDTO) {
         return personService.update(personDTO);
     }
 
-    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @DeleteMapping(value = "/{id}",
+            produces =
+            {MediaType.APPLICATION_JSON_VALUE,
+             MediaType.APPLICATION_XML_VALUE,
+             MediaType.APPLICATION_YAML_VALUE})
     public ResponseEntity<?> delete(@PathVariable("id") Long id) {
          personService.delete(id);
          return ResponseEntity.noContent().build();
--- src/main/java/com/reynan/spring/rest_with_spring_boot_end_java_enrudio/data/dto/PersonDTO.java
+++ src/main/java/com/reynan/spring/rest_with_spring_boot_end_java_enrudio/data/dto/PersonDTO.java
@@ -1,43 +1,17 @@
 package com.reynan.spring.rest_with_spring_boot_end_java_enrudio.data.dto;
 
-//import com.fasterxml.jackson.annotation.JsonIgnore;
-//import com.fasterxml.jackson.annotation.JsonProperty;
-//import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
-import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.reynan.spring.rest_with_spring_boot_end_java_enrudio.serializer.GenderSerializer;
-
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.util.Objects;
 
-// @JsonPropertyOrder({"id", "address", "firstName", "lastName", "gender"})
-@JsonFilter("PersonFilter")
+
 public class PersonDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private Long id;
-
-//    @JsonProperty("first_name")
     private String firstName;
-
-//    @JsonProperty("last_name")
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String lastName;
-
-    @JsonInclude(JsonInclude.Include.NON_EMPTY)
-    private String phoneNumber;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
-    private LocalDate birthDay;
-
     private String address;
-//    @JsonIgnore
-    @JsonSerialize(using = GenderSerializer.class)
     private String gender;
 
     public PersonDTO() {}
@@ -66,23 +40,6 @@ public class PersonDTO implements Serializable {
         this.lastName = lastName;
     }
 
-    public String getPhoneNumber() {
-
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public LocalDate getBirthDay() {
-        return birthDay;
-    }
-
-    public void setBirthDay(LocalDate birthDay) {
-        this.birthDay = birthDay;
-    }
-
     public String getAddress() {
         return address;
     }
@@ -102,13 +59,14 @@ public class PersonDTO implements Serializable {
     @Override
     public boolean equals(Object o) {
 
+
         if (o == null || getClass() != o.getClass()) return false;
         PersonDTO personDTO = (PersonDTO) o;
-        return Objects.equals(id, personDTO.id) && Objects.equals(firstName, personDTO.firstName) && Objects.equals(lastName, personDTO.lastName) && Objects.equals(phoneNumber, personDTO.phoneNumber) && Objects.equals(birthDay, personDTO.birthDay) && Objects.equals(address, personDTO.address) && Objects.equals(gender, personDTO.gender);
+        return Objects.equals(id, personDTO.id) && Objects.equals(firstName, personDTO.firstName) && Objects.equals(lastName, personDTO.lastName) && Objects.equals(address, personDTO.address) && Objects.equals(gender, personDTO.gender);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstName, lastName, phoneNumber, birthDay, address, gender);
+        return Objects.hash(id, firstName, lastName, address, gender);
     }
 }
